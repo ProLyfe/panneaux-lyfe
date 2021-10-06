d9e1c7a95303e10b08074a1af861a6a232edf9d8
ProLyfe <rayane.prolyfe@gmail.com>
2021-10-06 21:27:54 +0200

    [fix] replace svg by png

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test</Text>
+      <Image source={require('./assets/panneaux/interdiction/B1.png')} style={styles.imagePanneau}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +20,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imagePanneau: {
+    width: 190, 
+    height: 190,
+  }
 });
