7f71619898587806fed81496e29e20f33dddbe24
ProLyfe <rayane.prolyfe@gmail.com>
2021-10-07 16:38:51 +0200

    [add] obligation panneaux

---
+++ panneaux/danger.js
@@ -0,0 +1,30 @@
+import img1 from '../assets/panneaux/danger/A1a.png';
+import img2 from '../assets/panneaux/danger/A1b.png';
+import img3 from '../assets/panneaux/danger/A1c.png';
+import img4 from '../assets/panneaux/danger/A1d.png';
+
+
+const panneauxDanger = [
+    {
+        id: 1,
+        image: img1,
+        description: "xxxxx",
+    },
+    {
+        id: 2,
+        image: img2,
+        description: "xxxxx",
+    },
+    {
+        id: 3,
+        image: img3,
+        description: "xxxxxx",
+    },
+    {
+        id: 4,
+        image: img4,
+        description: "xxxxx",
+    },
+];
+
+export default panneauxDanger;
--- panneaux/obligation.js
+++ panneaux/obligation.js
@@ -1,58 +1,161 @@
-import obligation_droite from '../assets/panneaux/obligation/B21-1.png';
-import obligation_gauche from '../assets/panneaux/obligation/B21-2.png';
-import obligation_rabattre_droite from '../assets/panneaux/obligation/B21a1.png';
-import obligation_rabattre_gauche from '../assets/panneaux/obligation/B21a2.png';
+import img1 from '../assets/panneaux/obligation/B21-1.png';
+import img2 from '../assets/panneaux/obligation/B21-2.png';
+import img3 from '../assets/panneaux/obligation/B21a1.png';
+import img4 from '../assets/panneaux/obligation/B21a2.png';
+
+import img5 from '../assets/panneaux/obligation/B21b.png';
+import img6 from '../assets/panneaux/obligation/B21c1.png';
+import img7 from '../assets/panneaux/obligation/B21c2.png';
+import img8 from '../assets/panneaux/obligation/B21d1.png';
+
+import img9 from '../assets/panneaux/obligation/B21d2.png';
+import img10 from '../assets/panneaux/obligation/B21e.png';
+import img11 from '../assets/panneaux/obligation/B22a.png';
+import img12 from '../assets/panneaux/obligation/B22b.png';
+
+import img13 from '../assets/panneaux/obligation/B22c.png';
+import img14 from '../assets/panneaux/obligation/B25.png';
+import img15 from '../assets/panneaux/obligation/B26.png';
+import img16 from '../assets/panneaux/obligation/B27a.png';
+
+import img17 from '../assets/panneaux/obligation/B27b.png';
+import img18 from '../assets/panneaux/obligation/B29.png';
+import img19 from '../assets/panneaux/obligation/B40.png';
+import img20 from '../assets/panneaux/obligation/B41.png';
+
+import img21 from '../assets/panneaux/obligation/B42.png';
+import img22 from '../assets/panneaux/obligation/B43.png';
+import img23 from '../assets/panneaux/obligation/B44.png';
+import img24 from '../assets/panneaux/obligation/B45a.png';
+
+import img25 from '../assets/panneaux/obligation/B49.png';
+
 
 const panneauxObligation = [
     {
         id: 1,
-        image: obligation_droite,
-        description: "Obligation de tourner à droite",
+        image: img1,
+        description: "Obligation de tourner à droite avant le panneau",
     },
     {
         id: 2,
-        image: obligation_gauche,
-        description: "Obligation de tourner à gauche",
+        image: img2,
+        description: "Obligation de tourner à gauche avant le panneau",
     },
     {
         id: 3,
-        image: obligation_rabattre_droite,
-        description: "Obligation de se rabattre à droiteke lkzke j,zkej kzjekzjekzjekzjkejzkejkzekj",
+        image: img3,
+        description: "Contournement obligatoire de l'obstacle par la droite",
     },
     {
         id: 4,
-        image: obligation_rabattre_gauche,
-        description: "Obligation de se rabattre à gauche",
+        image: img4,
+        description: "Contournement obligatoire de l'obstacle par la gauche",
+    },
+    {
+        id: 5,
+        image: img5,
+        description: "Direction obligatoire à la prochaine intersection : tout droit",
+    },
+    {
+        id: 6,
+        image: img6,
+        description: "Direction obligatoire à la prochaine intersection : à droite",
+    },
+    {
+        id: 7,
+        image: img7,
+        description: "Direction obligatoire à la prochaine intersection : à gauche",
+    },
+    {
+        id: 8,
+        image: img8,
+        description: "Direction obligatoire à la prochaine intersection : tout droit ou à droite",
+    },
+    {
+        id: 9,
+        image: img9,
+        description: "Direction obligatoire à la prochaine intersection : tout droit ou à gauche",
+    },
+    {
+        id: 10,
+        image: img10,
+        description: "Direction obligatoire à la prochaine intersection : à gauche ou à droite",
+    },
+    {
+        id: 11,
+        image: img11,
+        description: "Piste ou bande obligatoire aux cycles sans side-car à 2 ou 3 roues",
+    },
+    {
+        id: 12,
+        image: img12,
+        description: "Chemin obligatoire pour piétons",
+    },
+    {
+        id: 13,
+        image: img13,
+        description: "Chemin obligatoire pour cavaliers",
+    },
+    {
+        id: 14,
+        image: img14,
+        description: "Vitesse minimale obligatoire",
+    },
+    {
+        id: 15,
+        image: img15,
+        description: "Chaînes à neige obligatoire sur au moins 2 roues motrices",
+    },
+    {
+        id: 16,
+        image: img16,
+        description: "Voie reservée aux véhicules des services réguliers de transport en commun",
+    },
+    {
+        id: 17,
+        image: img17,
+        description: "Voie reservée aux tramways",
+    },
+    {
+        id: 18,
+        image: img18,
+        description: "Obligation dont la nature est mentionnée sur le panneau",
+    },
+    {
+        id: 19,
+        image: img19,
+        description: "Fin de piste ou de bande cyclable",
+    },
+    {
+        id: 20,
+        image: img20,
+        description: "Fin de chemin obligatoire pour piétons",
+    },
+    {
+        id: 21,
+        image: img21,
+        description: "Fin de chemin obligatoire pour cavaliers",
+    },
+    {
+        id: 22,
+        image: img22,
+        description: "Fin de vitesse minimale obligatoire",
+    },
+    {
+        id: 23,
+        image: img23,
+        description: "Fin de l'obligation de l'usage des chaînes à neige",
     },
     {
         id: 24,
-        image: obligation_rabattre_gauche,
-        description: "Obligation de se rabattre à gauche",
+        image: img24,
+        description: "Fin de voie réservée aux véhicules des services réguliers de transport en commun",
     },
     {
-        id: 224,
-        image: obligation_rabattre_gauche,
-        description: "Obligation de se rabattre à gauche",
-    },
-    {
-        id: 2224,
-        image: obligation_rabattre_gauche,
-        description: "Obligation de se rabattre à gauche",
-    },
-    {
-        id: 344,
-        image: obligation_rabattre_gauche,
-        description: "Obligation de se rabattre à gauche",
-    },
-    {
-        id: 234,
-        image: obligation_rabattre_gauche,
-        description: "Obligation de se rabattre à gauche",
-    },
-    {
-        id: 1314,
-        image: obligation_rabattre_gauche,
-        description: "Obligation de se rabattre à gauche",
+        id: 25,
+        image: img25,
+        description: "Fin d'obligation d'allumage des feux",
     },
 ];
 
