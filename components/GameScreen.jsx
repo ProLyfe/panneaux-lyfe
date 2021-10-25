0075b22364461a59137cbaf264b3dae98133057c
ProLyfe <rayane.prolyfe@gmail.com>
2021-10-25 23:45:46 +0200

    [add] prepare recap screen & refacto GameScreen

---
+++ components/GameScreen.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import ToutPanneaux from './ToutPanneaux';
+import PanneauxInterdiction from './PanneauxInterdiction';
+import PanneauxObligation from './PanneauxObligation'; 
+import PanneauxIndication from './PanneauxIndication';
+import PanneauxIntersectionEtPriorite from './PanneauxIntersectionEtPriorite';
+import PanneauxDanger from './PanneauxDanger';
+import PanneauxServices from './PanneauxServices'
+
+
+const GameScreen = ({ navigation, score, questionNb, randomizedArray, keyId, questions, disabledButton, disabledButtonColor, verifyAnswer}) => (
+    <>
+       <View style={styles.headerOptions}>
+        <Text style={styles.headerText} onPress={() => navigation.goBack()}>Back</Text>
+        <Text style={styles.headerText}>Score : {score} pts</Text>
+        <Text style={styles.headerText}>{questionNb + 1} / {randomizedArray.length - 1}</Text>
+      </View>
+      <View style={styles.questionTextContainer}>
+        <Text style={styles.questionText}>A quoi correspond ce panneau ?</Text>
+      </View>
+
+      <View style={styles.panneauxContainer}>
+        {
+          keyId === 1 ? <ToutPanneaux id={randomizedArray[questionNb].id} width={190} height={190}/> :
+          keyId === 2 ? <PanneauxInterdiction id={randomizedArray[questionNb].id} width={190} height={190}/> :
+          keyId === 3 ? <PanneauxObligation id={randomizedArray[questionNb].id} width={190} height={190}/> :
+          keyId === 4 ? <PanneauxIndication id={randomizedArray[questionNb].id} width={190} height={190}/> :
+          keyId === 5 ? <PanneauxIntersectionEtPriorite id={randomizedArray[questionNb].id} width={190} height={190}/> :
+          keyId === 6 ? <PanneauxDanger id={randomizedArray[questionNb].id} width={190} height={190}/> :
+          keyId === 7 ? <PanneauxServices id={randomizedArray[questionNb].id} width={190} height={190}/> : <Text>Rien</Text>
+        }
+      </View>
+      <View style={styles.answersContainer}>
+        {questions.map(question => <TouchableOpacity style={{...styles.answers, backgroundColor: disabledButtonColor}} disabled={disabledButton} onPress={() => verifyAnswer(question.status)} key={question.questionId}><View><Text style={styles.textAnswers}>{question.proposition}</Text></View></TouchableOpacity>)}
+      </View>
+    </>
+);
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+    },
+    headerOptions: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 20,
+      alignItems: 'center'
+    },
+    headerText: {
+      margin: 10,
+      color: 'white', 
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    panneauxContainer: {
+      width: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+      color: 'white',
+      textAlign: 'center'
+    },
+    questionText: {
+      color: 'white', 
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    questionTextContainer: {
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    answersContainer: {
+      display: 'flex',
+      width: '100%',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      alignItems: 'center',  
+    },
+    answers: {
+      height: '45%',
+      width: '45%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: '2%',
+      borderRadius: 20,
+    },
+    textAnswers: {
+      color: 'white',
+      textAlign: 'center',
+    }
+  });
+
+export default GameScreen;
+
--- screens/modals/ModalQuizz.jsx
+++ screens/modals/ModalQuizz.jsx
@@ -1,13 +1,6 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import ToutPanneaux from '../../components/ToutPanneaux';
-import PanneauxInterdiction from '../../components/PanneauxInterdiction';
-import PanneauxObligation from '../../components/PanneauxObligation'; 
-import PanneauxIndication from '../../components/PanneauxIndication';
-import PanneauxIntersectionEtPriorite from '../../components/PanneauxIntersectionEtPriorite';
-import PanneauxDanger from '../../components/PanneauxDanger';
-import PanneauxServices from '../../components/PanneauxServices'
-import useRandom from '../../hooks/useRandom';
+import { StyleSheet, Text, View } from 'react-native';
+import GameScreen from '../../components/GameScreen';
 
 const ModalQuizz = ({ navigation, route }) => {
 
@@ -17,19 +10,20 @@ const ModalQuizz = ({ navigation, route }) => {
   const [disabledButton, setDisabledButton] = useState(false);
   const [disabledButtonColor, setDisabledButtonColor] = useState('#1C2753');
 
-  const key = route.params.key;
-  const randomizedArray = route.params.panneaux;
+  const keyId = route.params.key;
+  // const randomizedArray = route.params.panneaux;
+  const randomizedArray = [...route.params.panneaux, {id: 'yaey'}]
   
   // const questions = useRandom(randomizedArray[questionNb].questions);
   const questions = randomizedArray[questionNb].questions;
 
-  const verifyAnswer = (answer, panneauId) => {
+  const verifyAnswer = (answer) => {
     if (answer === true) {
       setBgColor('green');
       setDisabledButton(true);
       setDisabledButtonColor('#121213')
+      setScore(prevScore => prevScore + 1);
         setTimeout(() => {
-          setScore(prevScore => prevScore + 1);
           setQuestionNb(prevState => prevState + 1);
           setBgColor('#04103A');
           setDisabledButton(false);
@@ -40,7 +34,7 @@ const ModalQuizz = ({ navigation, route }) => {
       setDisabledButton(true);
       setDisabledButtonColor('#121213')
         setTimeout(() => {
-        setQuestionNb(prevState => prevState + 1);
+        // setQuestionNb(prevState => prevState + 1);
         setBgColor('#04103A');
         setDisabledButton(false);
         setDisabledButtonColor('#1C2753')
@@ -50,29 +44,18 @@ const ModalQuizz = ({ navigation, route }) => {
 
   return (
     <View style={{...styles.container, backgroundColor: bgColor}}>
-      <View style={styles.headerOptions}>
-        <Text style={styles.headerText} onPress={() => navigation.goBack()}>Back</Text>
-        <Text style={styles.headerText}>Score : {score} pts</Text>
-        <Text style={styles.headerText}>{questionNb} / {randomizedArray.length}</Text>
-      </View>
-      <View style={styles.questionTextContainer}>
-        <Text style={styles.questionText}>A quoi correspond ce panneau ?</Text>
-      </View>
 
-      <View style={styles.panneauxContainer}>
-        {
-          key === 1 ? <ToutPanneaux id={randomizedArray[questionNb].id} width={190} height={190}/> :
-          key === 2 ? <PanneauxInterdiction id={randomizedArray[questionNb].id} width={190} height={190}/> :
-          key === 3 ? <PanneauxObligation id={randomizedArray[questionNb].id} width={190} height={190}/> :
-          key === 4 ? <PanneauxIndication id={randomizedArray[questionNb].id} width={190} height={190}/> :
-          key === 5 ? <PanneauxIntersectionEtPriorite id={randomizedArray[questionNb].id} width={190} height={190}/> :
-          key === 6 ? <PanneauxDanger id={randomizedArray[questionNb].id} width={190} height={190}/> :
-          key === 7 ? <PanneauxServices id={randomizedArray[questionNb].id} width={190} height={190}/> : <Text>Rien</Text>
-        }
-      </View>
-      <View style={styles.answersContainer}>
-        {questions.map((question, index) => <TouchableOpacity style={{...styles.answers, backgroundColor: disabledButtonColor}} disabled={disabledButton} onPress={() => verifyAnswer(question.status, randomizedArray[questionNb].id)} key={question.questionId}><View><Text style={styles.textAnswers}>{question.proposition}</Text></View></TouchableOpacity>)}
-      </View>
+      { randomizedArray[questionNb].id !== 'yaey' ? <GameScreen 
+        navigation={navigation} 
+        score={score} 
+        questionNb={questionNb} 
+        randomizedArray={randomizedArray} 
+        keyId={keyId} 
+        questions={questions}
+        disabledButton={disabledButton}
+        disabledButtonColor={disabledButtonColor}
+        verifyAnswer={verifyAnswer}
+      /> : <Text>Rien poto</Text>}
     </View>
   );
 };
@@ -83,62 +66,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-evenly',
   },
-  headerOptions: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 20,
-    alignItems: 'center'
-  },
-  headerText: {
-    margin: 10,
-    color: 'white', 
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  panneauxContainer: {
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    textAlign: 'center'
-  },
-  questionText: {
-    color: 'white', 
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  questionTextContainer: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  answersContainer: {
-    display: 'flex',
-    width: '100%',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',  
-  },
-  answers: {
-    height: '45%',
-    width: '45%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: '2%',
-    borderRadius: 20,
-  },
-  textAnswers: {
-    color: 'white',
-    textAlign: 'center',
-  }
 });
 
 export default ModalQuizz;
